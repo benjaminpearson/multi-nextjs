aa4d5326abdfcf822e45f53809586370180b2d7a
Benjamin Pearson <benjaminpearson@users.noreply.github.com>
2021-02-07 22:39:19 +1100

    Move catch all page to src folder

--- pages/[[...catch]].js
+++ pages/[[...catch]].js
@@ -1,10 +1,1 @@
-// [[catch]].js is an optional catch all route https://github.com/vercel/next.js/pull/12887
-
-import React from 'react';
-import Error from 'next/error';
-
-function Catch() {
-  return <Error statusCode={404} />
-}
-
-export default Catch;
+export { default } from 'errors/404';
